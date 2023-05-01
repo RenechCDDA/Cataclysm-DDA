a314d5a6df3c6e3f8a3da42b366683ec5287d2eb
RenechCDDA <84619419+RenechCDDA@users.noreply.github.com>
2023-05-01 12:05:24 -0400

    karol'd

--- src/iexamine.h
+++ src/iexamine.h
@@ -111,6 +111,7 @@ void shrub_wildveggies( Character &you, const tripoint &examp );
 void part_con( Character &you, const tripoint &examp );
 void water_source( Character &, const tripoint &examp );
 void finite_water_source( Character &, const tripoint &examp );
+void digester_empty( Character &you, const tripoint &examp );
 void kiln_empty( Character &you, const tripoint &examp );
 void kiln_full( Character &you, const tripoint &examp );
 void arcfurnace_empty( Character &you, const tripoint &examp );
